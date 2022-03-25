2322074de7be54db7f5e45b5cf3c113114781f1e
Valeriy Nehovora <nehovora.val@gnail.com>
2022-03-24 22:55:34 -0400

    useEffect dependency array

--- tripago/src/components/TripList.jsx
+++ tripago/src/components/TripList.jsx
@@ -5,12 +5,13 @@ import "./TripList.css";
 
 export default function TripList() {
   const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
 
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    fetch(url)
       .then(response => response.json())
       .then(json => setTrips(json));
-  }, []);
+  }, [url]);
 
   console.log(trips);
 
@@ -25,6 +26,23 @@ export default function TripList() {
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => {
+            setUrl("http://localhost:3000/trips?loc=europe");
+          }}
+        >
+          Evropean Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=america")}
+        >
+          American Trips
+        </button>
+      </div>
     </div>
   );
 }
