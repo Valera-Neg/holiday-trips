bd2abd57437ee98a7613c7bed6b4a75726d79cb3
Valeriy Nehovora <nehovora.val@gnail.com>
2022-03-29 21:43:51 -0400

    using aboartController

--- tripago/src/hooks/useFetch.jsx
+++ tripago/src/hooks/useFetch.jsx
@@ -4,12 +4,15 @@ export const useFetch = url => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
+
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchData = async () => {
       setIsPending(true);
 
       try {
-        const res = await fetch(url);
+        const res = await fetch(url, { signal: controller.signal });
         if (!res.ok) {
           throw new Error(res.statusText);
         }
@@ -19,13 +22,20 @@ export const useFetch = url => {
         setData(json);
         setError(null);
       } catch (err) {
-        setIsPending(false);
-        setError("Could not fetch the data");
-        console.log(err.message);
+        if (err.name === "AbortError") {
+          console.log("The featch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+        }
       }
     };
 
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
   return { data, isPending, error };
